362449bbd83eebaa98b29edbfb3039190d962b09
Leo BESNARD <lbesnard@e2r8p16.clusters.42paris.fr>
2022-03-22 18:16:30 +0100

    fdf debut

---
+++ Makefile
@@ -0,0 +1,47 @@
+SRCDIR = src/
+OBJDIR = bin/
+INCDIR = include/
+FTPATH = libft/
+MLPATH = mlx_linux/
+INDIR = $(FTPATH)include/
+LIBFT = $(FTPATH)libft.a
+MLX =$(MLPATH)libmlx.a
+
+#################################################################
+src = main.c
+
+OBJS = ${addprefix $(OBJDIR),$(src:.c=.o)}
+#################################################################
+CFLAGS = -Wall -Wextra -Werror 
+LINK = -lft -lmlx -lXext -lX11 -lm -lz
+CC = gcc -g
+INCPATH = -I$(INCDIR) -I$(INDIR) -I$(MLPATH)
+LIBPATH = -L$(FTPATH) -L$(MLPATH)
+NAME = fdf
+
+all: ${NAME}
+
+$(OBJDIR)%.o: $(SRCDIR)%.c
+	$(CC) -c $(CFLAGS) $(INCPATH) $< -o $@
+
+${NAME}: ${OBJS} $(LIBFT) $(MLX)
+	$(CC) ${OBJS} $(LIBPATH) $(LINK) -o $(NAME)
+
+$(LIBFT):
+	make -C $(FTPATH)
+
+$(MLX):
+	make -C $(MLPATH)
+checker:
+	make re -C checker_dir/
+	cp checker_dir/checker .
+
+clean:
+	rm -f ${OBJS}
+
+fclean: clean
+	rm -f ${NAME}
+
+re: fclean all
+
+.PHONY: all libft clean fclean re
---
+++ src/main.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbesnard <lbesnard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 15:54:02 by lbesnard          #+#    #+#             */
+/*   Updated: 2022/03/22 18:14:44 by lbesnard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+	int i;
+	int y;
+
+	i = 480;
+	y = 270;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	while (y < 810)
+	{
+		while (i++ < 1440)
+		{
+			my_mlx_pixel_put(&img, i, y, 0x00FF0000);
+		}
+		y++;
+		i= 480;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
